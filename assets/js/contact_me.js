991fa4d0883ff0ad92b9aefac4ef1e1ff3e77279
onur <67955086+otuva@users.noreply.github.com>
2025-11-24 16:43:44 +0300

    add subject

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -14,6 +14,7 @@ $(function() {
       var name = $("input#name").val();
       var email = $("input#email").val();
       var phone = $("input#phone").val();
+      var subject = $("input#subject").val();
       var message = $("textarea#message").val();
       var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
@@ -30,6 +31,7 @@ $(function() {
           name: name,
           phone: phone,
           email: email,
+          subject: subject,
           message: message
         },
         cache: false,
